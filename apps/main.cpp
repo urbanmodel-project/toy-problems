ef26c8c1ea1ed137444b4ecca32bcc90176aaf74
Gautam Bisht <gautam.bisht@pnnl.gov>
2025-11-01 19:45:12 -0700

    Each MPI rank provides information about cuda

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,9 +1,16 @@
 #include <Kokkos_Core.hpp>
 #include "kokkos_utils/vector_add.hpp"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
+#ifdef KOKKOS_ENABLE_CUDA
+    std::cout << "Rank: " << (std::getenv("PMI_RANK") ? std::getenv("PMI_RANK") : "0")
+              << " using device: " << Kokkos::Cuda().cuda_device_prop().name
+              << " (device " << Kokkos::Cuda().cuda_device() << ")"
+              << std::endl;
+#endif
     kokkos_utils::vector_add(10);
   }
   Kokkos::finalize();
