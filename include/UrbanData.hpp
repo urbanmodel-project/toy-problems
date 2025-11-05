fbde2c4a9aa682eb471acfce1863880ce1dbc4db
Gautam Bisht <gautam.bisht@pnnl.gov>
2025-11-05 14:05:39 -0800

    Renaming things

--- include/UrbanData.hpp
+++ include/UrbanData.hpp
@@ -12,30 +12,39 @@ using ExecutionSpace = Kokkos::DefaultExecutionSpace;
 
 // --- Fortran Type: urbanparams_vars and Canyon Geometry ---
 struct CanyonGeometryData {
-  Array1DR8 canyon_hwr; // ratio of building height to street width
-  Array1DR8 vf_sr;      // view factor of sky for road
-  Array1DR8 vf_sw;      // view factor of sky for one wall
-  Array1DR8 vf_rw;      // view factor from road to wall
-  Array1DR8 vf_ww;      // view factor from wall to opposing wall
+  HostArray1DI4 CanyonHwrH; // ratio of building height to street width
+  Array1DR8 CanyonHwr;      // ratio of building height to street width
+
+  Array1DR8 ViewFactorSkyForRoad;      // view factor of sky for road
+  Array1DR8 ViewFactorSkyForWall;      // view factor of sky for one wall
+  Array1DR8 ViewFactorRoadToWall;      // view factor from road to wall
+  Array1DR8 ViewFactorWallToOtherWall; // view factor from wall to opposing wall
 };
 
 // --- Local Variables and Initial Inputs ---
 struct SolarInputData {
-  Array1DR8 coszen; // cosine solar zenith angle (coszen)
-  Array2DR8 sdir_h; // direct beam solar radiation on horizontal surface (sdir)
-  Array2DR8 sdif_h; // diffuse solar radiation on horizontal surface (sdif)
-  Array1DR8 frac_sno; // fraction of ground covered by snow (col_ws%frac_sno)
+  HostArray1DR8 CoszenH; // cosine solar zenith angle
+  Array1DR8 Coszen;      // cosine solar zenith angle
+
+  HostArray2DR8 SdirHorizH; // direct beam solar radiation on horizontal surface
+  Array2DR8 SdirHoriz;      // direct beam solar radiation on horizontal surface
+
+  HostArray2DR8 SdifHorizH; // diffuse solar radiation on horizontal surface
+  Array2DR8 SdifHoriz;      // diffuse solar radiation on horizontal surface
+
+  HostArray1DR8 FracSnowH; // fraction of ground covered by snow
+  Array1DR8 FracSnow;      // fraction of ground covered by snow
 };
 
 // --- Intermediate/Output Fluxes (Shared between subroutines) ---
 struct AlbedoFluxes {
   // Incident Fluxes (Output of incident_direct/diffuse)
-  Array2DR8 sdir_road;      // direct incident on road
-  Array2DR8 sdif_road;      // diffuse incident on road
-  Array2DR8 sdir_sunwall;   // direct incident on sunlit wall
-  Array2DR8 sdif_sunwall;   // diffuse incident on sunlit wall
-  Array2DR8 sdir_shadewall; // direct incident on shaded wall
-  Array2DR8 sdif_shadewall; // diffuse incident on shaded wall
+  Array2DR8 SdirRoad;      // direct incident on road
+  Array2DR8 SdifRoad;      // diffuse incident on road
+  Array2DR8 SdirSunwall;   // direct incident on sunlit wall
+  Array2DR8 SdifSunwall;   // diffuse incident on sunlit wall
+  Array2DR8 SdirShadewall; // direct incident on shaded wall
+  Array2DR8 SdifShadewall; // diffuse incident on shaded wall
 
   // Snow-corrected albedos (Output of SnowAlbedo)
   Array2DR8 alb_roof_dir_s;
@@ -45,11 +54,11 @@ struct AlbedoFluxes {
 
   // Absorbed Fluxes (Output of net_solar) (solarabs_vars)
   Array2DR8 sabs_improad_dir; // direct solar absorbed by impervious road
-  Array2DR8 sabs_sunwall_dir; // direct solar absorbed by sunwall
+  Array2DR8 sabsSunwall_dir;  // direct solar absorbed by sunwall
 
   // Reflected Fluxes (Output of net_solar) (Local sref_... variables)
-  Array2DR8 sref_roof_dir;    // direct solar reflected by roof (sref_roof_dir)
-  Array2DR8 sref_sunwall_dir; // direct solar reflected by sunwall
+  Array2DR8 sref_roof_dir;   // direct solar reflected by roof (sref_roof_dir)
+  Array2DR8 srefSunwall_dir; // direct solar reflected by sunwall
 };
 
 // --- The Master Bundle passed between all classes ---
--- src/UrbanDataAllocator.cpp
+++ src/UrbanDataAllocator.cpp
@@ -18,17 +18,18 @@ void UrbanDataAllocator::allocate_all_views() const {
          N_LUN, N_RAD);
 
   // --- CanyonGeometryData Allocation ---
-  data_bundle.geometry.canyon_hwr = Array1DR8("canyon_hwr", N_LUN);
-  data_bundle.geometry.vf_sr = Array1DR8("vf_sr", N_LUN);
+  data_bundle.geometry.CanyonHwr = Array1DR8("CanyonHwr", N_LUN);
+  data_bundle.geometry.ViewFactorSkyForRoad =
+      Array1DR8("ViewFactorSkyForRoad", N_LUN);
   // ... all other geometry views ...
 
   // --- SolarInputData Allocation ---
   data_bundle.input.coszen = Array1DR8("coszen", N_LUN);
-  data_bundle.input.sdir_h = Array2DR8("sdir_h", N_LUN, N_RAD);
+  data_bundle.input.SdirHoriz = Array2DR8("SdirHoriz", N_LUN, N_RAD);
   // ... all other input views ...
 
   // --- AlbedoFluxes Allocation ---
-  data_bundle.fluxes.sdir_road = Array2DR8("sdir_road", N_LUN, N_RAD);
+  data_bundle.fluxes.SdirRoad = Array2DR8("SdirRoad", N_LUN, N_RAD);
   // ... all other flux views ...
 
   printf("All primary Views successfully allocated on device.\n");
