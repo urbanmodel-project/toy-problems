c1554497eb912dc31c6ab426a963d20546e72ead
Gautam Bisht <gautam.bisht@pnnl.gov>
2025-12-28 10:27:00 -0800

    Fixes macro for fortran

--- include/finclude/UrbanMacros.h
+++ include/finclude/UrbanMacros.h
@@ -2,13 +2,13 @@
 #define URBAN_MACROS_F90
 
 #define URBAN_SUCCESS 0
-#define URBAN_CALL_OR_STOP(stmt)                                               \
-  call stmt;                                                                   \
-  if (status /= URBAN_SUCCESS)                                                 \
-    then;                                                                      \
-  print *, 'Urban API call failed: ', status;                                  \
-  stop 1;                                                                      \
+#define URBAN_CALL_OR_STOP(stmt) \
+  call stmt
+#define CallA(stmt) \
+  URBAN_CALL_OR_STOP(stmt); \
+  if (status /= URBAN_SUCCESS) then; \
+    print *, 'API call failed: ', status; \
+    stop 1; \
   end if
-#define CallA(stmt) URBAN_CALL_OR_STOP(stmt)
 
 #endif /* URBAN_MACROS_F90 */
